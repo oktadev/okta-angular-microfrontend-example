7eccafc2fa2c563c2445dbd6b968f02d04906f80
Alisa Duncan <7586237+alisaduncan@users.noreply.github.com>
2022-04-29 16:32:05 -0500

    all scaffolding

---
+++ projects/mfe-basket/src/app/basket/basket.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BasketRoutingModule } from './basket-routing.module';
+import { BasketComponent } from './basket.component';
+
+
+@NgModule({
+  declarations: [
+    BasketComponent
+  ],
+  imports: [
+    CommonModule,
+    BasketRoutingModule
+  ]
+})
+export class BasketModule { }
